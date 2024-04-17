6b4bc4d35f3625a065d800a710fbb900c35fc14f
CptPenguin <32851658+CptPenguin@users.noreply.github.com>
2024-04-17 16:27:50 +0200

    section3 pause at 46 part

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            {CORE_CONCEPTS.map((coreConcept) => {
+              return (
+                <CoreConcept
+                  title={coreConcept.title}
+                  description={coreConcept.description}
+                  img={coreConcept.image}
+                />
+              );
+            })}
+          </ul>
+        </section>
       </main>
     </div>
+  );
+}
+
+const reactDescriptions = ["Fundamental", "Core", "Crucial"];
+
+function getRandomDescription(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomDescription(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
   );
 }
 
